e70970674ae7d97904a8bea42b2c47d16d93ea5c
piyushsingh05 <impiyushkumar5@gmail.com>
2026-04-24 23:40:47 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/securityscanner/security_scanner/SecurityScannerApplication.java
@@ -0,0 +1,13 @@
+package com.securityscanner.security_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securityscanner/security_scanner/SecurityScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityscanner.security_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
